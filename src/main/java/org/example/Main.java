329fac21dc575e4b65e23cf37530780d4472bd4d
Vincent Adeola <vince.adeola@outlook.com>
2023-03-28 22:37:22 +0100

    fixes firstName setter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        BankAccount bankAccount = new BankAccount("Vincent", "Adeola", "31/07/2001", 9103910);
+        bankAccount.setFirstName("Bill");
+        System.out.println(bankAccount.getFirstName());
     }
 }
+
