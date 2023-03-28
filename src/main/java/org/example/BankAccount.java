de238e26e7ae487cb8fc2e6f0bc417944b111cb0
Vincent Adeola <vince.adeola@outlook.com>
2023-03-28 21:46:26 +0100

    adds deposit + withdrawl methods

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -1,5 +1,31 @@
 package org.example;
 
 public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private int accountNumber;
+    private int balance;
+
+    public BankAccount(String inputFirstName, String inputLastName, String inputDateOfBirth, int inputAccountNumber){
+        this.firstName = inputFirstName;
+        this.lastName = inputLastName;
+        this.dateOfBirth = inputDateOfBirth;
+        this.accountNumber = inputAccountNumber;
+        this.balance = 0;
+    }
+
+    public int deposit(int amount){
+        return this.balance + amount;
+    }
+
+    public int withdrawal(int amount){
+        return this.balance - amount;
+    }
+// Generous Bank
+    public int interest(){
+        return (this.balance * o.25) + this.balance;
+    }
+
 
 }
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -2,6 +2,6 @@ import org.junit.jupiter.api.Test;
 
 public class BankAccountTest {
     @Test
-
+    public
 
 }
